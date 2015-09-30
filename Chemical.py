8101e94f256d86718bce9d8dc5734a0f9278193b
NandaJS <nandasheelvant@gmail.com>
2015-09-30 12:44:11 +0530

    Initial commit
    Saving the 1st version of this chemical program

---
+++ Chemical.py
@@ -0,0 +1,15 @@
+def take_input():
+    level = (input("Enter the current level of ethenol in the tank:"))
+    return level
+
+def action(level):
+    if level>(900.0/100)*80:
+        print "close the valve"
+    else:
+        if level<(900.0/100)*20:
+            print "buy more liquid"
+        else:
+            print "level of ethenol in the tank is",level
+    
+level=take_input()
+action(level)
